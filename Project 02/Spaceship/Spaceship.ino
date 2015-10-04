fbaa18862d4d2024f1d9823e1f8a17889b551b6a
David Siew <davidsiew@outlook.com>
2015-10-04 12:45:52 +0200

    Project 2. Green Led will be on and Red Leds will be off until switch is pressed. No blinking

---
+++ Project 02/Spaceship/Spaceship.ino
@@ -0,0 +1,29 @@
+int switchState = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, INPUT); // Switch  
+  
+  pinMode(3, OUTPUT); // Green Led
+  pinMode(4, OUTPUT); // Red Led  
+  pinMode(5, OUTPUT); // Red Led  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  switchState = digitalRead(2);
+
+  // Button is not pressed
+  if(switchState == LOW)
+  {
+    digitalWrite(3, HIGH); // green LED
+    digitalWrite(4, LOW); // red LED
+    digitalWrite(5, LOW); // red LED
+  }
+  else // Button is pressed
+  {
+    digitalWrite(3, LOW); // green LED
+    digitalWrite(4, HIGH); // red LED
+    digitalWrite(5, HIGH); // red LED
+  }
+}
